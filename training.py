e2b0ed1da62e51ed8f2743b578a5904187d47fa6
blazinbanana <4808.2021@students.ku.ac.ke>
2026-02-06 11:25:56 +0300

    added link to license

--- training.py
+++ training.py
@@ -1,33 +1,5 @@
-# **AI Lab: Deep Learning for Computer Vision**
-# **WorldQuant University**
-#
-#
-
-# **Usage Guidelines**
-#
-# This file is licensed under Creative Commons Attribution-NonCommercial-
-# NoDerivatives 4.0 International.
-#
-# You **can** :
-#
-#   * ✓ Download this file
-#   * ✓ Post this file in public repositories
-#
-# You **must always** :
-#
-#   * ✓ Give credit to WorldQuant University for the creation of this file
-#   * ✓ Provide a link to the license
-#
-# You **cannot** :
-#
-#   * ✗ Create derivatives or adaptations of this file
-#   * ✗ Use this file for commercial purposes
-#
-# Failure to follow these guidelines is a violation of your terms of service and
-# could lead to your expulsion from WorldQuant University and the revocation
-# your certificate.
-#
-#
+#credit to WorldQuant University for the creation of this file
+#    https://creativecommons.org/licenses/by-nc-nd/4.0/
 
 from collections import Counter
 
